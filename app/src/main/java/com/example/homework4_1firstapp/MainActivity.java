464daf385286fc5852b1a0e6e85d29fc9693cb0a
sherruh <sherruh@gmail.com>
2019-02-15 19:56:01 +0600

    add buttons

--- app/src/main/java/com/example/homework4_1firstapp/MainActivity.java
+++ app/src/main/java/com/example/homework4_1firstapp/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.homework4_1firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnIntent;
+    Button btnHideIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIntent=findViewById(R.id.intent_btn);
+        btnHideIntent=findViewById(R.id.hide_intent_btn);
+
+        btnIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("click1","clicked on Intent");
+            }
+        });
+    }
+
+    public void onHideClick(View v){
+        Log.d("click2","clicked on Hide Intent");
     }
 }
