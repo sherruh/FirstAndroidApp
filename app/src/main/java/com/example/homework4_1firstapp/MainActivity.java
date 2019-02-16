57040141463c32e8f03d835c35f26db3aea38b87
sherruh <sherruh@gmail.com>
2019-02-16 23:41:37 +0600

    clear editText

--- app/src/main/java/com/example/homework4_1firstapp/MainActivity.java
+++ app/src/main/java/com/example/homework4_1firstapp/MainActivity.java
@@ -4,14 +4,19 @@ import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnIntent;
     Button btnHideIntent;
+    EditText editText;
+    String textFromEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,24 @@ public class MainActivity extends AppCompatActivity {
 
         btnIntent=findViewById(R.id.intent_btn);
         btnHideIntent=findViewById(R.id.hide_intent_btn);
+        editText=findViewById(R.id.textEdit);
+
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                textFromEdit=editText.getText().toString();
+            }
+        });
 
         btnIntent.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +51,13 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent=new Intent(MainActivity.this,Activity2.class);
                 intent.putExtra("SOME_TEXT","Text from main activity");
                 startActivityForResult(intent,1);
+            }
+        });
+
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText("");
             }
         });
     }
